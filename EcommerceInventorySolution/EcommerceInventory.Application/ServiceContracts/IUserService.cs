6e829b3dd11821d4b30ffbf32064e548500dd413
Albert <abogevorgian@gmail.com>
2025-09-29 12:23:14 +0400

    feat: add IUserService interface and implement UserService
    - Introduced IUserService in ServiceContracts for user-related operations.
    - Implemented UserService with methods for user management.
    - Integrated UserService with  IUnitOfWOrk.

--- EcommerceInventorySolution/EcommerceInventory.Application/ServiceContracts/IUserService.cs
+++ EcommerceInventorySolution/EcommerceInventory.Application/ServiceContracts/IUserService.cs
@@ -1,4 +1,11 @@
-﻿namespace EcommerceInventory.Application.ServiceContracts;
+﻿using EcommerceInventory.Domain.Entities;
+
+namespace EcommerceInventory.Application.ServiceContracts;
 public interface IUserService
 {
+
+    Task<User> RegisterUserAsync(string username, string password);
+    Task<Session> LoginAsync(string username, string password, string deviceId);
+    Task LogoutAsync(Guid userId, Guid sessionId);
+    Task LogoutAllAsync(Guid userId);
 }
--- EcommerceInventorySolution/EcommerceInventory.Application/Services/UserService.cs
+++ EcommerceInventorySolution/EcommerceInventory.Application/Services/UserService.cs
@@ -1,6 +1,73 @@
-﻿using EcommerceInventory.Application.ServiceContracts;
+﻿using EcommerceInventory.Application.RepositoryContracts;
+using EcommerceInventory.Application.ServiceContracts;
+using EcommerceInventory.Domain.Entities;
 
 namespace EcommerceInventory.Application.Services;
 public class UserService : IUserService
 {
+    private readonly IUnitOfWOrk _unitOfWork;
+
+    public UserService(IUnitOfWOrk unitOfWOrk)
+    {
+        _unitOfWork = unitOfWOrk;
+    }
+
+    public async Task<Session> LoginAsync(string username, string password, string deviceId)
+    {
+        var user = await _unitOfWork.Users.GetByUsernameAsync(username)
+            ?? throw new InvalidOperationException("Invalid username or password.");
+
+        if (!user.VerifyPassword(password))
+            throw new InvalidOperationException("Invalid username or password.");
+
+        var session = new Session(user.Id, deviceId, DateTime.UtcNow.AddHours(1));
+        user.AddSession(session);
+        
+        _unitOfWork.Users.Update(user);
+        await _unitOfWork.CompleteAsync();
+
+        return session;
+    }
+
+    public async Task LogoutAllAsync(Guid userId)
+    {
+        var user = await _unitOfWork.Users.GetByIdAsync(userId)
+            ?? throw new InvalidOperationException("User not found.");
+
+        user.RemoveAllSessions();
+
+        _unitOfWork.Users.Update(user);
+
+        await _unitOfWork.CompleteAsync();
+    }
+
+    public async Task LogoutAsync(Guid userId, Guid sessionId)
+    {
+        var user = await _unitOfWork.Users.GetByIdAsync(userId)
+            ?? throw new InvalidOperationException("User not found.");
+
+        var session = user.Sessions.FirstOrDefault(s => s.Id == sessionId);
+        if (session != null)
+        {
+            user.RemoveSession(session);
+            _unitOfWork.Users.Update(user);
+            await _unitOfWork.CompleteAsync();
+        }
+    }
+
+    public async Task<User> RegisterUserAsync(string username, string password)
+    {
+        if (await _unitOfWork.Users.GetByUsernameAsync(username) is not null)
+        {
+            throw new InvalidOperationException("Username already exists.");
+        }
+
+        var user = new User(username, password);
+
+        await _unitOfWork.Users.AddAsync(user);
+
+        await _unitOfWork.CompleteAsync();
+
+        return user;
+    }
 }
