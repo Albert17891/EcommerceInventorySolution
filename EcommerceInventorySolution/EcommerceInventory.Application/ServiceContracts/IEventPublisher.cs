5dbe785e2bc57bf31955bd6089ae0118221e409e
Albert <abogevorgian@gmail.com>
2025-09-30 11:16:55 +0400

    implemnt publisher for Outpox messages

---
+++ EcommerceInventorySolution/EcommerceInventory.Application/ServiceContracts/IEventPublisher.cs
@@ -0,0 +1,5 @@
+﻿namespace EcommerceInventory.Application.ServiceContracts;
+public interface IEventPublisher
+{
+    Task PublishAsync<T>(T @event) where T : class;
+}
---
+++ EcommerceInventorySolution/EcommerceInventory.Application/Services/OutboxEventPublisher.cs
@@ -0,0 +1,29 @@
+﻿using EcommerceInventory.Application.RepositoryContracts;
+using EcommerceInventory.Application.ServiceContracts;
+using Microsoft.Extensions.Logging;
+
+namespace EcommerceInventory.Application.Services;
+public class OutboxEventPublisher : IEventPublisher
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<OutboxEventPublisher> _logger;
+
+    public OutboxEventPublisher(IUnitOfWork unitOfWork, ILogger<OutboxEventPublisher> logger)
+    {
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+    public async Task PublishAsync<T>(T @event) where T : class
+    {
+        var eventType = typeof(T).Name;
+        var payload = System.Text.Json.JsonSerializer.Serialize(@event);
+
+        var outboxMessage = new Domain.Entities.OutboxMessage(eventType, payload);
+
+        await _unitOfWork.OutboxMessages.AddMessageAsync(outboxMessage);
+
+        await _unitOfWork.CompleteAsync();
+
+        _logger.LogInformation("Event {EventType} saved to outbox", eventType);
+    }
+}
