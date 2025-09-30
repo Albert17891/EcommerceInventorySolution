191631da2c32f5175e698e2d59ddda41391bea9e
Albert <abogevorgian@gmail.com>
2025-09-30 11:14:05 +0400

    add product mapping use automapper

---
+++ EcommerceInventorySolution/EcommerceInventory.Application/Mappings/ProductMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EcommerceInventory.Application.DTO.ProductDTO;
+using EcommerceInventory.Domain.Entities;
+
+namespace EcommerceInventory.Application.Mappings;
+public class ProductMappingProfile : Profile
+{
+    public ProductMappingProfile()
+    {
+        CreateMap<CreateProductRequestDto, Product>()
+            .ConstructUsing(dto => new Product(dto.Name, dto.Stock, dto.Price));
+
+        CreateMap<UpdateProductRequestDto, Product>()
+            .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+
+        CreateMap<Product, ProductResponseDto>();
+    }
+}
