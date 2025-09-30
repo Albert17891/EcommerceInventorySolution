cf1a44d0d835e2aaf86e17b14c8255553ffe46b9
Albert <abogevorgian@gmail.com>
2025-09-30 12:49:02 +0400

    add order controller

---
+++ EcommerceInventorySolution/EcommerceInventory.Api/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceInventory.Api.Attributes;
+using EcommerceInventory.Application.DTO.OrderDTO;
+using EcommerceInventory.Application.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceInventory.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpPost("PlaceOrder")]
+    [CustomAuthorize]
+    public async Task<IActionResult> PlaceOrder(Guid userId, List<OrderItemDto> items)
+    {
+        var order = await _orderService.CreateOrderAsync(userId, items);
+
+        return Ok(order);
+    }
+}
