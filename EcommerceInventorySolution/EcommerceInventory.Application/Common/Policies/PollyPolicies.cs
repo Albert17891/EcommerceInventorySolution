2d8b8b622efae206826ce557f8a867bd95b4c25a
Albert <abogevorgian@gmail.com>
2025-09-30 11:14:47 +0400

    implement policies for retry use polly package

---
+++ EcommerceInventorySolution/EcommerceInventory.Application/Common/Policies/PollyPolicies.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Polly;
+
+namespace EcommerceInventory.Application.Common.Policies;
+public static class PollyPolicies
+{
+    public static IAsyncPolicy CreateConcurrencyRetryPolicy()
+    {
+        return Policy
+            .Handle<DbUpdateConcurrencyException>()
+            .WaitAndRetryAsync(
+                retryCount: 3,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromMilliseconds(100 * retryAttempt)
+            );
+    }
+}
