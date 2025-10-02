3b4380cd1734565405be2310739723fb63b612d5
Gevorkyan Albert <abogevorgian@gmail.com>
2025-10-02 12:12:56 +0400

    add new migrations file update DiscountRule Entity

---
+++ EcommerceInventorySolution/EcommerceInventory.Infrastructure/Migrations/20251002075046_UpdateEntities.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceInventory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MinimumPurchaseAmount",
+                table: "DiscountRules",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FixedAmount",
+                table: "DiscountRules",
+                type: "numeric(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DiscountPercentage",
+                table: "DiscountRules",
+                type: "numeric(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardType",
+                table: "DiscountRules",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscountRules_CardType",
+                table: "DiscountRules",
+                column: "CardType",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DiscountRules_CardType",
+                table: "DiscountRules");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MinimumPurchaseAmount",
+                table: "DiscountRules",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FixedAmount",
+                table: "DiscountRules",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DiscountPercentage",
+                table: "DiscountRules",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardType",
+                table: "DiscountRules",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
